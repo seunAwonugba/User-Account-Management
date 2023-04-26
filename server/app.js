aa986b39d6fc909cf28d48b0774a5e7f6130fb64
seun awonugba <86803598+seunAwonugba@users.noreply.github.com>
2023-04-26 18:59:30 +0100

    implement backend code structure

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const { StatusCodes, ReasonPhrases } = require("http-status-codes");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 8000;
 const host = process.env.HOST || "localhost";
+
+app.use(cors);
 
 app.get("/", (req, res) => {
     return res.status(StatusCodes.OK).json({
