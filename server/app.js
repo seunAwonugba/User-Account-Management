a3a6e51824771aaf830dd6d316fffc5178cf66c8
seun awonugba <86803598+seunAwonugba@users.noreply.github.com>
2023-04-26 19:24:38 +0100

    connect app to database

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const { StatusCodes, ReasonPhrases } = require("http-status-codes");
 const cors = require("cors");
+
+const { sequelize } = require("./models/index");
 
 const app = express();
 const port = process.env.PORT || 8000;
@@ -15,6 +18,24 @@ app.get("/", (req, res) => {
     });
 });
 
-app.listen(port, host, () => {
-    console.log(`Server is listening on http://${host}:${port}`);
-});
+const startServer = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log(
+            "⚡️[database]: Database connection has been established successfully."
+        );
+
+        app.listen(port, host, () => {
+            console.log(
+                `⚡️[server]: Server is listening on http://${host}:${port}`
+            );
+        });
+    } catch (error) {
+        console.error(
+            "😥 [database]: Unable to connect to the database:",
+            error
+        );
+    }
+};
+
+startServer();
