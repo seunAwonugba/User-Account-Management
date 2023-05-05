9f03dae4949229ea4bc4b4ae6636a4a35b483297
seun awonugba <86803598+seunAwonugba@users.noreply.github.com>
2023-05-05 10:17:14 +0100

    implement auth middleware

--- client/src/page/Profile.js
+++ client/src/page/Profile.js
@@ -1,3 +1,82 @@
+import * as React from "react";
+import TextField from "@mui/material/TextField";
+import {
+    Avatar,
+    Button,
+    FormControl,
+    Grid,
+    InputLabel,
+    OutlinedInput,
+    Paper,
+    Typography,
+} from "@mui/material";
+import baseUrl from "../base_url/base-url";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 export default function Profile() {
-    return <div className="message-screen">Profile screen</div>;
+    const navigate = useNavigate();
+
+    const paperStyle = { padding: "30px 20px", width: 550 };
+    const marginStyle = { marginTop: "15px" };
+
+    const [firstName, setFirstName] = React.useState("");
+    const [lastName, setLastName] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    const [confirmPassword, setConfirmPassword] = React.useState("");
+    const [isLoading, setIsLoading] = React.useState("");
+
+    const signUp = async (e) => {
+        e.preventDefault();
+        setIsLoading(true);
+
+        const userResponse = {
+            firstName,
+            lastName,
+            email,
+            password,
+            repeat_password: confirmPassword,
+        };
+
+        try {
+            const response = await baseUrl.post(
+                "/auth/create-user",
+                userResponse
+            );
+            setIsLoading(false);
+
+            if (response.data.success === true) {
+                navigate("/email-confirmation-sent");
+            } else {
+                setIsLoading(false);
+                toast.error(response.data.data);
+            }
+        } catch (error) {
+            setIsLoading(false);
+
+            toast.error(error.response.data.data);
+        }
+    };
+    return isLoading ? (
+        <body>
+            <h4>Loading...</h4>
+        </body>
+    ) : (
+        <Grid>
+            <Paper elevation={20} style={paperStyle}>
+                <Grid align="center">
+                    <Grid>
+                        <Avatar></Avatar>
+
+                        <h2>Profile Page</h2>
+                        <Typography variant="caption">
+                            Complete this form to sign up!
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Paper>
+        </Grid>
+    );
 }
---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const { Unauthenticated } = require("../error");
+
+const authMiddleware = async (req, res, next) => {
+    var authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        throw new Unauthenticated("Auth token is required for this route");
+    }
+
+    const token = authHeader.split(" ")[1].trim();
+
+    try {
+        //verify you got the correct token, it returns the obj in your signed token
+        const decode = jwt.verify(token, process.env.JWT_SECRET);
+
+        //if verification is successfully, set up a property on the request object, call it user, and pass it to the next middleware which it the next route after logging in
+
+        //req.user can be accessed in any route that is authenticated
+        req.user = {
+            id: decode.id,
+            name: decode.name,
+            email: decode.email,
+        };
+        next();
+    } catch (err) {
+        throw new Unauthenticated("Invalid or expired token");
+    }
+};
+
+//export it to all the routes you want to authenticate
+module.exports = { authMiddleware };
